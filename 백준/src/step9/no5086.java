36eaf6c42af6d3f2bb6060f007e86598ac3dff41
zong-ahri <choijoy10@naver.com>
2024-01-04 21:38:05 +0900

    no message

---
+++ 백준/src/step9/no5086.java
@@ -0,0 +1,17 @@
+package step9;
+
+import java.util.*;
+
+public class no5086 {
+
+	public static void main(String[] args) {
+		// 배수와 약수
+		// 배수와 약수를 배우는 문제
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		
+
+	}
+
+}
